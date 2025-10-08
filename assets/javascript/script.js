c4787e30bebab3d4199fd56ece222d3235a6b1ac
Renan Ribeiro <rphiliper@gmail.com>
2025-10-08 12:39:06 -0300

    Fix links and remove unnecessary comments

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,10 @@
-async function addElement(idCatch, idInsert) {
+
+async function addElement(arquivo, idCatch, idInsert) {
   try {
-    const r = await fetch("https://raw.githubusercontent.com/RenanPhilip/RenanPhilip/main/index.html");
+    const host = window.location.host;
+    const protocol = window.location.protocol;
+
+    const r = await fetch(`${protocol}//${host}/${arquivo}`);
     const html = await r.text();
     const temp = document.createElement("div");
     temp.innerHTML = html;
@@ -20,55 +24,16 @@ async function addElement(idCatch, idInsert) {
 
 // ✅ agrupa os fetches e espera todos terminarem
 const elementos = [
-  ['navbar', 'navbar'],
-  ['menu', 'contatos-grid'],
-  ["nome", "nome"],
-  ["especialidade", "especialidade"],
-  ["headline", "headline"],
-  ["resumo-curriculo", "resumo"],
-  ["certificacoes", "certificacoes-container"],
-  ["experiencia", "experiencia-section"]
+  ['/RenanPhilip/shared/navbar_components/navbar.html', 'navbar-menu', 'contatos-grid'],
+  ['RenanPhilip/index.html', "nome", "nome"],
+  ['RenanPhilip/index.html', "especialidade", "especialidade"],
+  ['RenanPhilip/index.html', "headline", "headline"],
+  ['RenanPhilip/index.html', "resumo-curriculo", "resumo"],
+  ['RenanPhilip/index.html', "certificacoes", "certificacoes-container"],
+  ['RenanPhilip/index.html', "experiencia", "experiencia-section"]
 ];
 
-Promise.all(elementos.map(([a, b]) => addElement(a, b)))
+Promise.all(elementos.map(([a, b, c]) => addElement(a, b, c)))
   .then(() => {
     console.log("Todos os elementos carregados.");
-    const alvo = document.querySelector("#nome");
-    if (alvo) alvo.scrollIntoView({ behavior: "smooth", block: "start" });
   });
-
-// Navbar Toggle
-document.addEventListener("DOMContentLoaded", () => {
-
-    const toggle = document.getElementById("menu-toggle");
-    const menu = document.getElementById("menu");
-
-    // Toggle abr/fecha (evita propagação)
-    toggle.addEventListener("click", (e) => {
-        menu.classList.toggle("show");
-        e.stopPropagation();
-    });
-
-    // Fecha ao clicar fora
-    document.addEventListener("click", (e) => {
-        if (menu.classList.contains("show")) {
-            if (!menu.contains(e.target) && !toggle.contains(e.target)) {
-                menu.classList.remove("show");
-            }
-        }
-    });
-
-    // Fecha com ESC
-    document.addEventListener("keydown", (e) => {
-        if (e.key === "Escape") menu.classList.remove("show");
-    });
-});
-
-// Name Smooth
-window.addEventListener("load", () => {
-    console.log('window load')
-    const alvo = document.querySelector("#nome");
-    if (alvo) {
-        alvo.scrollIntoView({ behavior: "smooth", block: "start" });
-    }
-});
